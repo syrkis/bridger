b8fabe56964929ab676003df40e6268b9d916813
Tim <timpoulsen1@gmail.com>
2021-04-21 09:40:35 +0200

    Change reader to not take a random sample from each data file, as to speed it up

--- bin/reader.py
+++ bin/reader.py
@@ -4,7 +4,6 @@ import json
 import gzip
 import random
 
-# SOME FILES ARE LARGE SO LOADING WHOLE FILE IS STILL SLOW, FIND WAY TO SAMPLE LINES WITHOUT READING WHOLE FILE
 
 #next line gives file not found when run in singularity
 data_root = '/home/common/datasets/amazon_review_data_2018'
@@ -26,7 +25,7 @@ def sample_data(number_of_files, size_of_files, seed = None):
 			- Number of files to sample from. Will be randomly selected	
 
 		size_of_files int
-			- Number of reviews to sample from each data file. Will be chosen randomly
+			- Number of reviews to sample from each data file. Will not be chosen randomly, is picked as the first 'size_of_files' reviews
 
 		seed: int
 			- Seed to set for random collection
@@ -43,16 +42,24 @@ def sample_data(number_of_files, size_of_files, seed = None):
 	core_data_files = os.listdir(core_path)
 	if number_of_files > len(core_data_files) or number_of_files == -1:
 		number_of_files = len(core_data_files)
+
+	if size_of_files == -1:
+		size_of_files = float('inf') 
+
 	files_to_use = random.sample(core_data_files, number_of_files)
 	data, names = [], []
 	for core_file in files_to_use: 
 		base_file_name = core_file[:-10]
 		full_path = os.path.join(core_path, core_file)
 		print('READING %s' % base_file_name)
-		current_file = [line for line in parse(full_path)]
-		if size_of_files > len(current_file) or size_of_files == -1:
-			size_of_files = len(current_file)
-		data_sample = random.sample(current_file, size_of_files)
+		
+		counter = 0
+		data_sample = []
+		for line in parse(full_path):
+			data_sample.append(line)
+			counter += 1
+			if counter >= size_of_files: break
+
 		data.append(data_sample)
 		names.append(base_file_name)
 
@@ -64,7 +71,7 @@ def read_all_files():
 	return sample_data(-1,-1)
 
 def main():
-	data,names = sample_data(1, 10)
+	data,names = sample_data(3, 10)
 	print(names)
 	print()
 	print(data)
