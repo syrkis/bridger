69b34ede123dc38d280d0162a68aa27800745000
Noah Syrkis <noah@syrkis.com>
2021-04-27 18:13:15 +0200

    non-word duplicating counter script

--- scripts/counts.sh
+++ scripts/counts.sh
@@ -1,7 +1,12 @@
 #! /usr/bin/zsh
 
 for F in $(ls /home/common/datasets/amazon_review_data_2018/reviews/); do
-	zcat /home/common/datasets/amazon_review_data_2018/reviews/$F | jq '.["reviewText"]' | tr 'a-z' 'A-Z' | tr -sC 'A-Z' | tr -s ' ' '\n' | sort | uniq -c | sort -n > /home/nobr/bridger/data/counts/$F.out
-	echo $F
+	if [ ! -f "/home/nobr/bridger/data/counts/$(echo $F | cut -d '.' -f 1).out" ]; then
+		zcat /home/common/datasets/amazon_review_data_2018/reviews/$F | jq '.["reviewText"]' | tr 'a-z' 'A-Z' | tr -sC 'A-Z' ' ' | tr -s ' ' '\n' | sort | uniq -c | sort -n > /home/nobr/bridger/data/counts/$(echo $F | cut -d '.' -f 1).out
+		echo "JUST DID $F"
+	else
+		echo "ALREADY DONE $F"	
+	fi
 done
 
+echo "GOD BLESS THE MACHINE"
