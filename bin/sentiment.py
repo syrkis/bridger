221816f5e9f1d1fe91a02070f0b6cf46fb0f5e08
Noah Syrkis <noah@syrkis.com>
2021-04-29 09:11:11 +0200

    prep for sentiment

---
+++ bin/sentiment.py
@@ -0,0 +1,20 @@
+# sentiment.py
+#   binary sentiment classification
+# by: Group B
+
+# imports
+import os
+import torch
+from torch import nn
+import genism
+
+# readings
+data_path = "../data/samples"
+samples = os.listdir(data_path)
+domains = []
+for sample in samples:
+    
+
+
+# model
+nn.LSTM
