f29634efab62e4912ad2c19619f80b52c6b98c2d
TimSP98 <timpoulsen1@gmail.com>
2021-05-11 09:12:07 +0200

    Fix model predict

--- bin/sentiment.py
+++ bin/sentiment.py
@@ -13,6 +13,7 @@ import numpy as np
 from tqdm import tqdm
 import os
 import time
+from copy import deepcopy
 import json
 from matplotlib import pyplot as plt
 from sklearn.metrics import classification_report
@@ -29,7 +30,7 @@ model = gensim.models.KeyedVectors.load_word2vec_format('data/twitter.bin', bina
 # declare nn
 class RNN(nn.Module):
         
-    batch_size = 32
+    batch_size = 12
 
     lstm_layers = 3
     lstm_dropout = 0.25
@@ -41,6 +42,8 @@ class RNN(nn.Module):
 
     def __init__(self, sentence_len):
         super(RNN, self).__init__()
+        self.best_state_dict = {}
+        self.state_dict_score = float('inf')
         self.sentence_len = sentence_len
 
         # Embedding
@@ -82,15 +85,18 @@ class RNN(nn.Module):
         logger.info(X.shape)
         optimizer = Adam(self.parameters())
         loss = nn.BCELoss()
+
         logger.info("INITIAL LOSS")
         L = [loss(self.predict_proba(X), y).item()]
         if x_dev is not None:
             L_dev = [loss(self.predict_proba(x_dev), y_dev).item()]
         logger.info("FINISH INITIAL LOSS")
+
         X_batches = torch.split(X,RNN.batch_size)
         if X_batches[-1].shape[0] != RNN.batch_size:
             X_batches = X_batches[:-1]
         y_batches = torch.split(y,RNN.batch_size)
+
         for e in range(E):
             losses = []
             for i in tqdm(range(len(X_batches))):
@@ -103,10 +109,15 @@ class RNN(nn.Module):
                 optimizer.step() 
                 losses.append(cross_entropy_loss.item())
                 torch.cuda.empty_cache()
+
             logger.info(f"EPOCH {e} LOSS: {np.mean(losses)}")
             L.append(np.mean(losses))
             if x_dev is not None:
                 L_dev.append(loss(self.predict_proba(x_dev),y_dev).item())
+                if L_dev[-1] > self.state_dict_score:
+                    self.state_dict_score = L_dev[-1]
+                    self.best_state_dict = deepcopy(self.state_dict())
+
         if x_dev is not None:
             return L, L_dev
         return L
@@ -118,7 +129,7 @@ class RNN(nn.Module):
     def predict_proba(self,X):
         self.eval() # sets model in evaluation mode, to not use dropout
         with torch.no_grad():
-            if device == 'cuda':
+            if str(device) == 'cuda':
                 res = torch.cuda.memory_reserved(0)
                 alo = torch.cuda.memory_allocated(0)
                 free_mem = res - alo
@@ -137,6 +148,13 @@ class RNN(nn.Module):
                 torch.cuda.empty_cache()
             self.train() # sets model back to training mode which is default
             return torch.cat(probas,dim=0) 
+
+    def save(path, save_current_state = False):
+        if save_current_state or not self.best_state_dict:
+            torch.save(self.state_dict(), path)
+            return
+        torch.save(self.best_state_dict, path)
+        
       
 def main(): 
     logger.info(f'is using {device}')
@@ -146,15 +164,17 @@ def main():
     x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=10**5)
     x_train, x_dev, y_train, y_dev = train_test_split(x_train,y_train, test_size=10**5)
     model = RNN(128).to(device) 
-    L, L_dev = model.fit(x_train, y_train, x_dev, y_dev, E=30)
+    L, L_dev = model.fit(x_train, y_train, x_dev, y_dev, E=7)
     try:
         logger.info(f"L is: {str(L)}")
         logger.info(f"L_dev is: {str(L_dev)}")
-        with open('/home/timp/repositories/bringo/data/train_losses_025_drop.csv',"w") as of:
+        with open('/home/timp/repositories/bringo/data/train_losses_025_drop1.csv',"w") as of:
             of.write(",".join(map(str,L))) 
             of.write("\n")
             of.write(",".join(map(str,L_dev))) 
-    except:
+        model.save('/home/timp/repositories/bringo/data/models/RNN025.pt')
+    except Exception as e:
+        logger.info('error occurred:\n\t-' + str(e))
         pass
     P = model.predict(x_test)
     print("\n",classification_report(y_test, P),"\n")
